e83d40cfe535ca43a613cd28e4d0abc6c3225318
winnie-byun <winnie.byun@groundx.xyz>
2021-05-28 17:29:37 +0900

    apply 1.0.18 with subprovider

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const bip39 = require("bip39");
 const ethJSWallet = require("ethereumjs-wallet");
 const hdkey = require("ethereumjs-wallet/hdkey");
-const debug = require("debug")("truffle-hdwallet-provider");
+const debug = require("debug")("@truffle/hdwallet-provider");
 const ProviderEngine = require("web3-provider-engine");
 const FiltersSubprovider = require("web3-provider-engine/subproviders/filters.js");
 const NonceSubProvider = require("web3-provider-engine/subproviders/nonce-tracker.js");
 const HookedSubprovider = require("web3-provider-engine/subproviders/hooked-wallet.js");
 const ProviderSubprovider = require("./subproviders/provider.js");
 const Web3 = require("web3");
+const Transaction = require("ethereumjs-tx");
 const ethUtil = require("ethereumjs-util");
 const Url = require("url");
-const Caver = require("caver-js");
 
 // This line shares nonce state across multiple provider instances. Necessary
 // because within truffle the wallet is repeatedly newed if it's declared in the config within a
@@ -113,13 +113,10 @@ class HDWalletProvider {
           } else {
             cb("Account not found");
           }
-          const caver = new Caver(provider)
-          caver.klay.accounts.signTransaction(
-            txParams,
-            pkey.toString("hex"),
-          ).then((result) => {
-            cb(null, result.rawTransaction)
-          })
+          const tx = new Transaction(txParams);
+          tx.sign(pkey);
+          const rawTx = `0x${tx.serialize().toString("hex")}`;
+          cb(null, rawTx);
         },
         signMessage({ data, from }, cb) {
           const dataIfExists = data;
@@ -191,7 +188,7 @@ HDWalletProvider.isValidProvider = provider => {
 
   if (typeof provider === "string") {
     const url = Url.parse(provider.toLowerCase());
-    return validProtocols.includes(url.protocol);
+    return validProtocols.includes(url.protocol) && url.slashes;
   }
 
   return true;
